2269490add1a2c2629c2fbb146a40c27be416ec2
Diego Alderete <141648675+killerchipy723@users.noreply.github.com>
2025-11-25 09:49:52 -0300

    okis radio

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/radio")
+def radiofm():
+     return render_template('fmrio.html')
+
 if __name__ == "__main__":
      app.run(debug=True, port=3200, host="0.0.0.0")
