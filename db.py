87b1794be2734a9308e5985ac7afd72a557f997d
killerchipy723 <diegoeduardo66@gmail.com>
2026-08-14 15:48:43 -0300

    ok inscripciones

--- db.py
+++ db.py
@@ -4,17 +4,17 @@ from pymysql.cursors import DictCursor
 
 def get_connection():
     return pymysql.connect(
-        #host="200.58.106.156",
-        #port=3306,
-        #user="c2710325_killer",
-        #password="SistemaIES6021",
-        #database="c2710325_muni",
-
-        host="localhost",
+        host="200.58.106.156",
         port=3306,
-        user="root",
-        password="inclub123",
-        database="muni",
+        user="c2710325_killer",
+        password="SistemaIES6021",
+        database="c2710325_muni",
+
+        #host="localhost",
+        #port=3306,
+        #user="root",
+        #password="inclub123",
+        #database="muni",
 
         charset="utf8mb4",
         cursorclass=DictCursor,
